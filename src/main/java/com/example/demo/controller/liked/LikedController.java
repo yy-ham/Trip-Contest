9958a8e7da97d07fc6edbfedf84f2f27b648fbc6
choimando <choihj2802@gmail.com>
2023-03-03 15:09:13 +0900

    3/3  관심목록-최효주

--- src/main/java/com/example/demo/controller/liked/LikedController.java
+++ src/main/java/com/example/demo/controller/liked/LikedController.java
@@ -9,23 +9,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 
 import com.example.demo.db.liked.DBManager;
 import com.example.demo.service.liked.LikedService;
 
-
+import jakarta.servlet.http.HttpSession;
 import lombok.Setter;
 
 
 @Controller
 @Setter
 public class LikedController {
-
-	@Autowired
-	private LikedService likedService;
 	
+	  @Autowired private LikedService likedService;
+	 
 	
 	
 	/*
@@ -38,27 +38,31 @@ public class LikedController {
 	 * return "/Liked/likeList"; }
 	 */
 	
-	@GetMapping("/Liked/likeList/{member_id}")
-	public ModelAndView list(@PathVariable String member_id) {
-		ModelAndView mav = new ModelAndView("/Liked/likeList");
-		mav.addObject("likedListPlan", likedService.findByIdandPlan(member_id));
-		mav.addObject("likedListTrip", likedService.findByIdandTrip(member_id));
-		return mav;
-	}
+	/* 나중에 쓸 것(session 아이디)
+	 * @GetMapping("/Liked/likeList") public ModelAndView list(HttpSession session)
+	 * { ModelAndView mav = new ModelAndView(); if(session.getAttribute("id") !=
+	 * null) { String member_id = (String)session.getAttribute("id");
+	 * mav.addObject("likedListPlan", likedService.findByIdandPlan(member_id));
+	 * mav.addObject("likedListTrip", likedService.findByIdandTrip(member_id));
+	 * mav.setViewName("Liked/likeList"); }else { mav.setViewName(""); } return mav;
+	 * }
+	 */
 	
 	
-	
-	
-	
-	@GetMapping("/Liked/deleteLiked/{like_no}")
-	public ModelAndView deleteLiked(@PathVariable int like_no) {
-		ModelAndView mav = new ModelAndView("redirect:/Liked/likeList");
-		likedService.deleteLiked(like_no);
-		mav.addObject("member_id", "hong");
-		mav.addObject("type", "plan");
-		mav.addObject("no", 1);
-		
-		return mav;
-	}
-	
+	  @GetMapping("/Liked/likeList/{member_id}") public ModelAndView
+	  list(@PathVariable String member_id) { ModelAndView mav = new
+	  ModelAndView("/Liked/likeList"); mav.addObject("likedListPlan",
+	  likedService.findByIdandPlan(member_id)); mav.addObject("likedListTrip",
+	  likedService.findByIdandTrip(member_id)); return mav; }
+	  
+	  
+	  
+	  @GetMapping("/Liked/deleteLiked/{like_no}")
+	  
+	  @ResponseBody public int deleteLiked(@PathVariable String like_no) { int no =
+	  Integer.parseInt(like_no); System.out.println(like_no); int re =
+	  likedService.deleteLiked(no);
+	  
+	  return re; }
+	 
 }
